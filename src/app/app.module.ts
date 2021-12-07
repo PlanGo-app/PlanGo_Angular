474fce5f2f0ed736e68076fb6db876fc55cf1def
Anthony Lafolie <anthony.lafolie.etu@univ-lille.fr>
2021-12-07 16:12:20 +0100

    change folder, resize image, and delete connection

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { AccueilComponent } from './accueil/accueil.component';
+
+@NgModule({
+	declarations: [
+		AppComponent,
+  AccueilComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		MatFormFieldModule,
+		MatInputModule, 
+		FormsModule, 
+		ReactiveFormsModule,
+		MatIconModule,
+		MatButtonModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
+})
+export class AppModule { }
