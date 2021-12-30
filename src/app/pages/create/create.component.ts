d8f41f5c7c8b581b43f1dc5b6ffc11ed7ea1beea
Anthony Lafolie <anthony.lafolie.etu@univ-lille.fr>
2021-12-30 13:52:00 +0100

    add dates validator

--- src/app/pages/create/create.component.ts
+++ src/app/pages/create/create.component.ts
@@ -15,7 +15,7 @@ export class CreateComponent implements OnInit {
 	dispError: boolean = false;
 
 
-	constructor(private travelService: TravelService, 
+	constructor(private travelService: TravelService,
 		private localTravelService: LocalTravelService,
 		private _snackBar: MatSnackBar,
 		public formBuilder: FormBuilder) { }
@@ -28,36 +28,56 @@ export class CreateComponent implements OnInit {
 			country: [null, [Validators.required]],
 			city: [null, [Validators.required]],
 			dateStart: [null, [Validators.required]],
-			dateEnd: [null, [Validators.required]],
-		})
+			dateEnd: [null, [Validators.required]]
+		},
+			{ validator: this.dateLessThan('dateStart', 'dateEnd') }
+		)
+	}
+
+	dateLessThan(from: string, to: string) {
+		return (group: FormGroup): { [key: string]: any } => {
+			let f = group.controls[from];
+			let t = group.controls[to];
+			if (f.value > t.value) {
+				t.setErrors({ 'incorrect': true });
+				f.setErrors({ 'incorrect': true });
+				return {
+					dates: "La date de fin doit être superieur à la date de début"
+				};
+			}
+			t.setErrors(null);
+			f.setErrors(null);
+			return {};
+		}
 	}
 
 	createTravel() {
-		if(!this.formCreate.valid){
+		if (!this.formCreate.valid) {
 			this.dispError = true;
 			this.formCreate.markAllAsTouched();
 			this.formCreate.markAsPristine();
-		}else{
-			this.travelService.createTravel(this.formCreate.value.country, 
-											this.formCreate.value.city, 
-											this.formCreate.value.dateStart, 
-											this.formCreate.value.dateEnd).subscribe({
-				next: (data) => {
-					console.log(data);
-					this.localTravelService.addTravel(data);
-				},
-				error: (err: HttpErrorResponse) => {
-					var errMsg = "";
-					switch (err.status) {
-						case 400:
-							errMsg = "Les informations données ne permettent pas de créer un voyage";
-							break;
+		} else {
+
+			this.travelService.createTravel(this.formCreate.value.country,
+				this.formCreate.value.city,
+				this.formCreate.value.dateStart,
+				this.formCreate.value.dateEnd).subscribe({
+					next: (data) => {
+						console.log(data);
+						this.localTravelService.addTravel(data);
+					},
+					error: (err: HttpErrorResponse) => {
+						var errMsg = "";
+						switch (err.status) {
+							case 400:
+								errMsg = "Les informations données ne permettent pas de créer un voyage";
+								break;
+						}
+						this._snackBar.open(errMsg, "Ok", { duration: 3000, panelClass: ['red-snackbar'] });
+
 					}
-					this._snackBar.open(errMsg,"Ok", {duration: 3000, panelClass: ['red-snackbar']});
-	
-				}
-			});
+				});
 		}
-					
+
 	}
 }
