d5f14bcf367fbdab6f0211389a345ecaa06a02cc
Anthony Lafolie <anthony.lafolie.etu@univ-lille.fr>
2021-12-30 15:52:31 +0100

    add form and required validator for code

--- src/app/pages/join/join.component.ts
+++ src/app/pages/join/join.component.ts
@@ -1,5 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { LocalTravelService } from 'src/app/services/travel-service/local-travel-service.component';
@@ -13,31 +14,46 @@ import { TravelService } from 'src/app/services/travel-service/travel-service.co
 export class JoinComponent implements OnInit {
 	invitationCode: string = "";
 	errorMsg: string = "";
-	constructor(private travelService: TravelService, private localTravelService: LocalTravelService, private router: Router,private _snackBar: MatSnackBar) { }
+	dispError: boolean = false;
+	constructor(private travelService: TravelService,
+		private localTravelService: LocalTravelService,
+		private router: Router,
+		private _snackBar: MatSnackBar,
+		public formBuilder: FormBuilder) { }
 
+	public formCreate!: FormGroup;
 	ngOnInit(): void {
+		this.formCreate = this.formBuilder.group({
+			code: [null, [Validators.required]],
+		});
 	}
 
 
 	join() {
-		this.travelService.joinTravel(this.invitationCode).subscribe({
-			next: (data) => {
-				console.log(data);
-				this.localTravelService.addTravel(data);
-				this.router.navigateByUrl("/");
-			},
-			error: (err: HttpErrorResponse) => {
-				var errMsg="";
-				switch (err.status) {
-					case 404:
-						errMsg = "Le code de voyage n'existe pas";
-						break;
-					case 409:
-						errMsg = "Vous êtes déjà dans ce voyage";
-						break;
+		if (!this.formCreate.valid) {
+			this.dispError = true;
+			this.formCreate.markAllAsTouched();
+			this.formCreate.markAsPristine();
+		} else {
+			this.travelService.joinTravel(this.formCreate.value.code).subscribe({
+				next: (data) => {
+					console.log(data);
+					this.localTravelService.addTravel(data);
+					this.router.navigateByUrl("/");
+				},
+				error: (err: HttpErrorResponse) => {
+					var errMsg = "";
+					switch (err.status) {
+						case 404:
+							errMsg = "Le code de voyage n'existe pas";
+							break;
+						case 409:
+							errMsg = "Vous êtes déjà dans ce voyage";
+							break;
+					}
+					this._snackBar.open(errMsg, "Ok", { duration: 3000, panelClass: ['red-snackbar'] });
 				}
-				this._snackBar.open(errMsg,"Ok", {duration: 3000, panelClass: ['red-snackbar']});
-			}
-		});
+			});
+		}
 	}
 }
