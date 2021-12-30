5ef0bde871e6510f751173ae2b42b8b874c5705d
Anthony Mendez <anthony.mendez.etu@univ-lille.fr>
2021-12-30 22:28:39 +0100

    update base url and include token in headers

--- src/app/services/travel-service/travel-service.component.ts
+++ src/app/services/travel-service/travel-service.component.ts
@@ -10,34 +10,45 @@ export class TravelService {
 
 	constructor(private http: HttpClient) { }
 
-	private baseUrl: string = "/plango-api/";
+	private baseUrl: string = "https://dashboard.heroku.com/";
 	private getTravelsUrl: string = "user/travels/";
 	private joinTravelUrl: string = "travel/invitation?code=";
 	private deleteTravelUrl: string = "travel/";
 	private createTravelUrl: string = "travel";
-	private token: string = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJQbGFuZ29Bbmd1bGFyIiwiaWF0IjoxNjM5MTQ4OTQxfQ.sSondsM8XuMajf6mpLnHPsFwgt9sJQqugKQ0Or_1Kp2meGkAV3Bq_jREjGI6K2QjsTNEdV_ryfLCQ2-Ldw7e2Q"
+	private token: string = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJQbGFuZ29Bbmd1bGFyIiwiaWF0IjoxNjM5MTQ4OTQxfQ.sSondsM8XuMajf6mpLnHPsFwgt9sJQqugKQ0Or_1Kp2meGkAV3Bq_jREjGI6K2QjsTNEdV_ryfLCQ2-Ldw7e2Q";
+	private httpOptions = {
+		headers: new HttpHeaders({
+		  'Content-Type':  'application/json',
+		  'Authorization': `Bearer ${this.token}`
+		})
+	  };
+
 
 	getTravels(): Observable<Travel[]> {
-		return this.http.get<TravelResponse>(this.baseUrl + this.getTravelsUrl).pipe(
+		return this.http.get<TravelResponse>(this.baseUrl + this.getTravelsUrl, this.httpOptions).pipe(
 			map((response) => response.travels));
 	}
 
 	joinTravel(code: string): Observable<Travel> {
-		return this.http.post<Travel>(this.baseUrl + this.joinTravelUrl + code, "");
+		return this.http.post<Travel>(this.baseUrl + this.joinTravelUrl + code, "", this.httpOptions);
 	}
 
-	deleteTravel(id: number): Observable<String> {
-		return this.http.delete(this.baseUrl + this.deleteTravelUrl + id + "/me", { responseType: 'text' });
+	deleteTravel(id: number): Observable<unknown> {
+		return this.http.delete(this.baseUrl + this.deleteTravelUrl + id + "/me", this.httpOptions);
 	}
 
 	createTravel(country: string, city: string, dateStart: Date, dateEnd: Date): Observable<Travel> {
-		return this.http.post<Travel>(this.baseUrl + this.createTravelUrl, 
-			{    
-			"name": "",
-			"country": country,
-			"city": city,
-			"dateStart": new Date(dateStart).toISOString(),
-			"dateEnd": new Date(dateEnd).toISOString() });
+		return this.http.post<Travel>(
+				this.baseUrl + this.createTravelUrl, 
+				{    
+					"name": "",
+					"country": country,
+					"city": city,
+					"dateStart": new Date(dateStart).toISOString(),
+					"dateEnd": new Date(dateEnd).toISOString() 
+				},
+				this.httpOptions
+			);
 	}
 
 
